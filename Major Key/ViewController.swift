4f8c68a8ebfe98401c391fe9c6808e05c4926d39
Felix Krause <krausefx@gmail.com>
2017-11-26 16:04:24 -0800

    Initial commit

---
+++ Major Key/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Major Key
+//
+//  Created by Felix Krause on 11/26/17.
+//  Copyright © 2017 Felix Krause. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
