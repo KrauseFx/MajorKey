0a5a2ee9f0d1103f2709ed1431453eabd2db2f3e
Felix Krause <krausefx@gmail.com>
2018-08-22 18:22:14 -0400

    Update Pods folder

--- Pods/Target Support Files/Pods-Major Key/Pods-Major Key-frameworks.sh
+++ Pods/Target Support Files/Pods-Major Key/Pods-Major Key-frameworks.sh
@@ -143,11 +143,9 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/SendGrid/SendGrid.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages/SwiftMessages.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/SendGrid/SendGrid.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages/SwiftMessages.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- Pods/Target Support Files/SendGrid/SendGrid-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SendGrid : NSObject
-@end
-@implementation PodsDummy_SendGrid
-@end
