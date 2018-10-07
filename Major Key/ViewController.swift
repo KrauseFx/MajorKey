bb155a77544e0ca8e4711e41d3e4e7c6731a6365
Felix Krause <krausefx@gmail.com>
2018-10-07 18:10:12 -0400

    Clear text field only after successfully sending an email

--- Major Key/ViewController.swift
+++ Major Key/ViewController.swift
@@ -54,9 +54,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         oldKeys!.append(text)
         UserDefaults.standard.set(oldKeys, forKey: defaultsForHistory)
         
-        triggerEmail(text: text)
-        
-        self.majorTextView.text = ""
+        triggerEmail(text: text)  
     }
     
     func showAlert(title: String, body: String, theme: Theme) {
@@ -127,6 +125,10 @@ class ViewController: UIViewController, UITextViewDelegate {
                 let responseString = String(data: data, encoding: .utf8)
                 print("responseString = \(String(describing: responseString))")
                 self.showAlert(title: "Major Key", body: "Never forget dat major key", theme: .success);
+                
+                DispatchQueue.main.async {
+                    self.majorTextView.text = ""
+                }
             }
             task.resume()
         } catch {
